95903806fffa24ef9b17c4c5adc972d9871db807
MuthiaPr <132433379+MuthiaPr@users.noreply.github.com>
2023-05-03 14:37:04 +0700

    Add files via upload

--- laptop.py
+++ laptop.py
@@ -7,12 +7,6 @@ df = pickle.load(open('estimasi_laptop_price.sav', 'rb'))
 
 st.title('Estimasi Harga Laptop')
 
-st.title("Estimasi Harga Laptop")
-
-company = st.selectbox('Brand',df['Company'].unique())
-
-type = st.selectbox('Type',df['TypeName'].unique())
-
 ram = st.selectbox('Ram_GB',[2, 4, 6, 8, 12, 16, 24, 32, 64])
 
 weight = st.number_input('Weight_KG')
@@ -21,19 +15,10 @@ touchscreen = st.selectbox('Touchscreen',['No', 'Yes'])
 
 ips = st.selectbox('IPS',['No', 'Yes'])
 
-inches = st.number_input('Inches')
-
-resolution = st.selectbox('ScreenResolution',[])
-
-cpu = st.selectbox('Cpu',df['Cpu_Brand'].unique())
-
 hdd = st.selectbox('HDD',[0, 128, 256, 512, 1024, 2048])
 
 ssd = st.selectbox('SSD',[0, 8, 128, 256, 512, 1024])
 
-gpu = st.selectbox('Gpu',df['Gpu_Brand'].unique())
-
-os = st.selectbox('OpSys',df['OS'].unique())
 
 if st.button('Predict price'):
     if touchscreen == 'Yes':
